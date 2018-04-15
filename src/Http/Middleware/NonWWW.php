403ac1ed113dc366c1edbd653911d67d9981849c
ReliQ <versatile.jamrok@gmail.com>
2018-04-15 08:28:41 -0500

    Laravel 5.6 Compatibility Update

--- src/Http/Middleware/NonWWW.php
+++ src/Http/Middleware/NonWWW.php
@@ -8,23 +8,21 @@ use Illuminate\Contracts\Routing\Middleware;
 /**
  * NonWWW
  * Redirects any www requests to non-www counterparts.
- * 
+ *
  * @param request The request object.
  * @param $next The next closure.
  * @return redirects to the secure counterpart of the requested uri.
  */
 class NonWWW
 {
-
     public function handle($request, Closure $next)
     {
-        if (substr($request->header('host'), 0, 4) == 'www.'){
-            $request->setTrustedProxies( [ $request->getClientIp() ] ); 
+        if (substr($request->header('host'), 0, 4) == 'www.') {
+            $request->setTrustedProxies([$request->getClientIp()], config('trustedproxy.headers'));
             $request->headers->set('host', parse_url(config('app.url'), PHP_URL_HOST));
             return redirect($request->getRequestUri(), 301);
         }
 
-        return $next($request); 
+        return $next($request);
     }
-	
 }
